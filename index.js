e3a1e4987ed9d96a1cd63f846d552f74c5a7d1b3
Tore <602123@stud.hvl.no>
2023-11-15 00:16:56 +0100

    Refactored code to improve performance and readability.

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-import { initializeApp } from 'firebase/app'
-import { getDatabase, ref, push, get, remove } from 'firebase/database'
+import { Configuration, OpenAIApi } from 'openai';
+import { process } from './env';
 
-import { Configuration, OpenAIApi } from 'openai'
-import { process } from './env'
-
+// Configuring OpenAI with the API key
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-})
+});
+const openai = new OpenAIApi(configuration);
 
-const openai = new OpenAIApi(configuration)
+const chatbotConversation = document.getElementById('chatbot-conversation');
+let conversationArr = []; // Array to store chat conversation locally
 
-const appSettings = {
-    databaseURL: 'https://dinnermate-18faa-default-rtdb.europe-west1.firebasedatabase.app//'
-}
-
-const app = initializeApp(appSettings)
-
-const database = getDatabase(app)
-
-const conversationInDb = ref(database)
-
-const chatbotConversation = document.getElementById('chatbot-conversation')
-
+// Instruction object for the chatbot
 const instructionObj = {
     role: 'system',
     content: 'You are a helpful cook that will suggest one dinner with recipe. Consider the user’s likes, dislikes, allergies, and the number of people they are cooking for. ',
-}
+};
 
 document.addEventListener('submit', (e) => {
     e.preventDefault();
+    
+    // Retrieving user inputs from the form
     const likesInput = document.getElementById('likes-input').value;
     const dislikesInput = document.getElementById('dislikes-input').value;
     const allergiesInput = document.getElementById('allergies-input').value;
@@ -36,77 +27,65 @@ document.addEventListener('submit', (e) => {
     
     const userInputCombined = `Likes: ${likesInput}, Dislikes: ${dislikesInput}, Allergies: ${allergiesInput}, Number of People: ${numberOfPeopleInput}`;
 
-    push(conversationInDb, {
+    // Adding user input to the local conversation array
+    conversationArr.push({
         role: 'user',
         content: userInputCombined
     });
 
     fetchReply();
     
-    const newSpeechBubble = document.createElement('div');
-    newSpeechBubble.classList.add('speech', 'speech-human');
-    chatbotConversation.appendChild(newSpeechBubble);
-    newSpeechBubble.textContent = userInputCombined;
-    chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
+    // Displaying user input in the chat interface
+    displaySpeechBubble(userInputCombined, 'human');
 });
 
 function fetchReply() {
-    get(conversationInDb).then(async (snapshot) => {
-        if (snapshot.exists()) {
-            const conversationArr = Object.values(snapshot.val())
-            conversationArr.unshift(instructionObj)
-            const response = await openai.createChatCompletion({
-                model: 'gpt-4-1106-preview',
-                messages: conversationArr,
-                presence_penalty: 0,
-                frequency_penalty: 0.3
-            })
-            push(conversationInDb, response.data.choices[0].message)
-            renderTypewriterText(response.data.choices[0].message.content)
-        }
-        else {
-            console.log('No data available')
-        }
+    // Prepending the instruction object to the conversation array
+    let conversationWithInstructions = [instructionObj, ...conversationArr];
 
-    })
+    openai.createChatCompletion({
+        model: 'gpt-4-1106-preview',
+        messages: conversationWithInstructions,
+        presence_penalty: 0,
+        frequency_penalty: 0.3
+    }).then(response => {
+        const replyText = response.data.choices[0].message.content;
+        // Adding the reply to the local conversation array
+        conversationArr.push(response.data.choices[0].message);
+        displaySpeechBubble(replyText, 'ai');
+    });
 }
 
-function renderTypewriterText(text) {
-    const newSpeechBubble = document.createElement('div')
-    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
-    chatbotConversation.appendChild(newSpeechBubble)
-    let i = 0
+function displaySpeechBubble(text, role) {
+    // Create a new speech bubble element
+    const newSpeechBubble = document.createElement('div');
+    newSpeechBubble.classList.add('speech', `speech-${role}`, 'blinking-cursor');
+    chatbotConversation.appendChild(newSpeechBubble);
+
+    // Call renderTypewriterText to display text with typewriter effect
+    renderTypewriterText(text, newSpeechBubble);
+}
+
+function renderTypewriterText(text, speechBubble) {
+    let i = 0;
     const interval = setInterval(() => {
-        newSpeechBubble.textContent += text.slice(i - 1, i)
+        speechBubble.textContent += text.slice(i, i + 1);
         if (text.length === i) {
-            clearInterval(interval)
-            newSpeechBubble.classList.remove('blinking-cursor')
+            clearInterval(interval);
+            speechBubble.classList.remove('blinking-cursor');
         }
-        i++
-        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-    }, 10)
+        i++;
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight;
+    }, 10);
 }
-
+// Event listener for the reset button
 document.getElementById('clear-btn').addEventListener('click', () => {
-    remove(conversationInDb)
-    chatbotConversation.innerHTML = '<div class="speech speech-ai">Im here to help you make dinner plans and find a recipe!</div>'
-})
+    // Clearing the chat from the page
+    chatbotConversation.innerHTML = '';
+    // Resetting the local conversation array
+    conversationArr = [];
+    // Optionally, you can display an initial message after clearing
+    displaySpeechBubble('I\'m here to help you make dinner plans and find a recipe!', 'ai');
+});
 
-function renderConversationFromDb(){
-    get(conversationInDb).then(async (snapshot)=>{
-        if(snapshot.exists()) {
-            Object.values(snapshot.val()).forEach(dbObj => {
-                const newSpeechBubble = document.createElement('div')
-                newSpeechBubble.classList.add(
-                    'speech',
-                    `speech-${dbObj.role === 'user' ? 'human' : 'ai'}`
-                    )
-                chatbotConversation.appendChild(newSpeechBubble)
-                newSpeechBubble.textContent = dbObj.content
-            })
-            chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-        }
-    })
-}
-
-renderConversationFromDb()
+// You can remove the renderConversationFromDb() function as it's no longer needed.
